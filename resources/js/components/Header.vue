f077dd821b799772a62b01bf25e5b34eb319333d
Gtp87 <fabio.giannitrapani@gmail.com>
2022-03-14 15:54:44 +0100

    create table leads

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,67 @@
+<template>
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <!-- <a class="navbar-brand" href="#">
+                <router-link class="nav-link" :to="{ name: 'home' }">
+                    <img id="logo" class="w-25" :src="logo" alt="logo"
+                /></router-link>
+            </a> -->
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li
+                        class="nav-item"
+                        v-for="item in menuItems"
+                        :key="item.id"
+                    >
+                        <!-- <a class="nav-link" :href="item.link">{{
+                            item.label
+                        }}</a> -->
+                        <router-link
+                            class="nav-link"
+                            :to="{ name: item.routeName }"
+                            >{{ item.label }}</router-link
+                        >
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    data() {
+        return {
+            // logo: require("../../img/home.png"),
+            menuItems: [
+                {
+                    label: "Home",
+                    routeName: "home",
+                },
+                {
+                    label: "Chi Siamo",
+                    routeName: "about",
+                },
+                {
+                    label: "Contacts",
+                    routeName: "contacts",
+                },
+            ],
+        };
+    },
+};
+</script>
+
+<style lang="scss"></style>
--- resources/js/front.js
+++ resources/js/front.js
@@ -8,9 +8,41 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+//import pages
 import App from './views/App';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contacts from './pages/Contacts';
+
+import VueRouter from 'vue-router';
+import Vue from 'vue';
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes:  [
+            {
+                path: '/',
+                name: 'home',
+                component: Home
+            },
+            {
+                path: '/about',
+                name: 'about',
+                component: About
+            },
+            {
+                path: '/contacts',
+                name: 'contacts',
+                component: Contacts
+            },
+            
+            
+        ]
+});
 
 const app = new Vue({
     el: '#app',
     render: h => h(App),
+    router
 });
